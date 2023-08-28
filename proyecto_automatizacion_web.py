94910415939af2d46e61f4118075e8a415e9e329
Olivia Luciano <olivialuciano2003@gmail.com>
2023-08-27 23:32:47 -0300

    final

--- proyecto_automatizacion_web.py
+++ proyecto_automatizacion_web.py
@@ -6,10 +6,6 @@ chromedriver_autoinstaller.install()
 import subprocess
 import os
 import json
-####### VER EL TEMA DE LOS PRECIOS CUANDO TIENEN DESCUENTO
-####### NO SÉ SI ESTAS PÁGINAS SON DINÁMICAS
-######## CARREFOUR CORREGIR PRECIOS CON MIL
-######## VER DE AGREGAR SECCION EJ ALMACEN, FREEZADOS
 
 # ARRANCAR
 print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
@@ -20,9 +16,6 @@ while start != 'S' and start != 'N':
     start = str(input("¿Desea buscar y comparar los precios de un producto en especifico de diferentes supermercados de la ciudad de rosario? s/n ")).upper()
 
 if start == 'S':
-    # ARCHIVO
-    #file = str(input("¿En donde desea crear el archivo del listado de productos? Por favor ingrese la ruta ")) ## CREAR ARCHIVO TEMPORAL
-
     # LOCACIÓN
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     location = str(input("¿En qué zona desea que se ubique el supermercado? (cualquiera: Q, centro: C, sur: S, oeste: O, fisherton: F, norte: N) ")).upper()
@@ -41,11 +34,10 @@ if start == 'S':
     products_data_jumbo = []
 
     # ############### 1 LA GALLEGA #################################
-
     def la_gallega_1(product):
-        he.start_chrome("https://www.lagallega.com.ar/login.asp")
         print("estamos buscando en La Gallega...")
-        #time.sleep(5)
+        he.start_chrome("https://www.lagallega.com.ar/login.asp")
+        time.sleep(6)
         search_box1 = he.find_all(he.S("#CabeBusca"))[0]
         he.wait_until(search_box1.exists)
         he.click(search_box1)
@@ -53,25 +45,23 @@ if start == 'S':
         he.wait_until(search_box12.exists)
         he.write(product, into=search_box12)
         he.press(he.ENTER)
-        time.sleep(2)
+        time.sleep(4)
         #list_of_cards = he.find_all(he.S(".cuadProd"))
-        #he.wait_until(list_of_cards.exists)
-        #len(list_of_cards)+1
-        try:
-            for i in range (1, 3) :
+        for i in range (1, 3) :
+            try:
                 product_name = (he.find_all(he.S(".desc"))[i-1]).web_element.text
                 product_price = (he.find_all(he.S(".izq"))[i-1]).web_element.text
-                products_data_la_gallega.append((product_name, product_price))
-        except:
-            print("No hemos encontrado el artículo.")
-            products_data_la_gallega.append("Artículo no encontrado")
+                products_data_la_gallega.append(("producto: " + product_name, "precio: " + product_price))
+            except:
+                print(f"No hemos encontrado el artículo {i}.")
+                products_data_la_gallega.append(f"Artículo {i} no encontrado")
         he.kill_browser()
 
     # # ############### 2 LA REINA ###################################
     def la_reina_2(product):
-        he.start_chrome("https://www.lareinaonline.com.ar/")
         print("estamos buscando en La Reina...")
-        #time.sleep(5)
+        he.start_chrome("https://www.lareinaonline.com.ar/")
+        time.sleep(6)
         search_box2 = he.find_all(he.S("#CabeBusca"))[0]
         he.wait_until(search_box2.exists)
         he.click(search_box2)
@@ -79,82 +69,65 @@ if start == 'S':
         he.wait_until(search_box22.exists)
         he.write(product, into=search_box22)
         he.press(he.ENTER)
-        time.sleep(2)
+        time.sleep(4)
         #list_of_cards = he.find_all(he.S(".cuadProd"))
-        try:
-            for i in range (1, 3) :
+        for i in range (1, 3) :
+            try:
                 product_name = (he.find_all(he.S(".desc"))[i-1]).web_element.text
                 product_price = (he.find_all(he.S(".izq"))[i-1]).web_element.text
-                products_data_la_reina.append((product_name, product_price))
-            #print(products_data_la_reina)
-        except:
-            print("No hemos encontrado el artículo.")
-            products_data_la_reina.append("Artículo no encontrado")
-        #acorto los productos a 2 porque hay productos cuyo precio esta en descuento y entonces cambia la clase css :( HAY QUE ARREGLAR
+                products_data_la_reina.append(("producto: " + product_name, "precio: " + product_price))
+            except:
+                print(f"No hemos encontrado el artículo {i}.")
+                products_data_la_reina.append(f"Artículo {i} no encontrado")
         he.kill_browser()
 
-    ############### 3 COTO #######################################    #NO PUDE EXTRAER EL NOMBRE DE LOS PRODUCTOS
+    ############### 3 COTO #######################################
     def coto_3(product):
+        print("estamos buscando en Coto...")
         he.start_chrome("https://www.cotodigital3.com.ar/sitios/cdigi/")
-        time.sleep(4)
+        time.sleep(6)
         search_box3 = he.find_all(he.S("#atg_store_searchInput"))[0]
         he.wait_until(search_box3.exists)
         he.write(product, into=search_box3)
         he.press(he.ENTER)
         time.sleep(6)
-    
-        # xPath porque coto hace imposible buscar un elemento porque todos los ids de ccs los hace distintos
-        # x_path_product_name = "//html/body/div[5]/div[2]/div/div/div[3]/div/div[1]/ul/li[3]/div[1]/div/a/span[2]/div/span/div/div[1]"
-        # x_path_product_price = "//html/body/div[5]/div[2]/div/div/div[3]/div/div[1]/ul/li[1]/div[2]/div[1]/div[1]/span/span"
-        # /html/body/div[5]/div[2]/div/div/div[3]/div/div[1]/ul/li[2]/div[2]/div[1]/div[1]/span/span
-        # /html/body/div[5]/div[2]/div/div/div[3]/div/div[1]/ul/li[3]/div[2]/div[1]/div[1]/span/span
-        # /html/body/div[5]/div[2]/div/div/div[3]/div/div[1]/ul/li[1]/div[2]/div[1]/div[1]/span/span
-
-        # "//html/body/div[5]/div[2]/div/div/div[3]/div/div[1]/ul/li[{i}]/div[1]/div/a/span[2]/div/span/div"
-        # /html/body/div[5]/div[2]/div/div/div[3]/div/div[1]/ul/li[2]/div[1]/div/a/span[2]/div/span/div
-        # /html/body/div[5]/div[2]/div/div/div[3]/div/div[1]/ul/li[3]/div[1]/div/a/span[2]/div/span/div/div[1]
-
-
         #list_of_cards = he.find_all(he.S(".clearfix"))
-        try:
-            for i in range (1, 3) :
+        for i in range (1, 3) :
+            try:
                 product_name = (he.find_all(he.S(f"//html/body/div[5]/div[2]/div/div/div[3]/div/div[1]/ul/li[{i}]/div[1]/div/a/span[2]/div/span/div"))[0]).web_element.text
                 product_price = (he.find_all(he.S(f"//html/body/div[5]/div[2]/div/div/div[3]/div/div[1]/ul/li[{i}]/div[2]/div[1]/div[1]/span/span"))[0]).web_element.text
-                products_data_coto.append((product_name, product_price))
-        except:
-            print("No hemos encontrado el artículo.")
-            products_data_coto.append("Artículo no encontrado")
+                products_data_coto.append(("producto: " + product_name, "precio: " + product_price))
+            except:
+                print(f"No hemos encontrado el artículo {i}.")
+                products_data_coto.append(f"Artículo {i} no encontrado")
         he.kill_browser()
 
-    # ############### 4 CARREFOUR ##################################  #downshift-1-input ##VER TEMA PRECIOS QUE APARECEN MAL
+    # ############### 4 CARREFOUR ##################################
     def carrefour_4(product):
-        he.start_chrome("https://www.carrefour.com.ar/")
         print("estamos buscando en Carrefour...")
+        he.start_chrome("https://www.carrefour.com.ar/")
         time.sleep(8)
-        search_box3 = he.find_all(he.S(".vtex-styleguide-9-x-input"))[0]
-        he.wait_until(search_box3.exists)
-        he.write(product, into=search_box3) 
-        he.press(he.ENTER) #tarda bastante en buscar la zanahoria y aparece abajo todo desubicado
+        search_box4 = he.find_all(he.S(".vtex-styleguide-9-x-input"))[0]
+        he.wait_until(search_box4.exists)
+        he.write(product, into=search_box4) 
+        he.press(he.ENTER)
         time.sleep(6)
         #list_of_cards = he.find_all(he.S(".valtech-carrefourar-product-summary-status-0-x-container"))
-        #len(list_of_cards)+1
-        try:
-            for i in range (1, 3) :
+        for i in range (1, 3) :
+            try:
                 product_name = (he.find_all(he.S(".vtex-product-summary-2-x-productNameContainer"))[i-1]).web_element.text
-                product_price_int = (he.find_all(he.S(".valtech-carrefourar-product-price-0-x-currencyInteger"))[i-1]).web_element.text
-                product_price_frac = (he.find_all(he.S(".valtech-carrefourar-product-price-0-x-currencyFraction"))[i-1]).web_element.text
-                products_data_carrefour.append((product_name, ("$ "+ product_price_int+ ","+ product_price_frac)))
-        except:
-            print("No hemos encontrado el artículo.")
-            products_data_carrefour.append("Artículo no encontrado")
-
+                product_price_all = (he.find_all(he.S(".valtech-carrefourar-product-price-0-x-sellingPriceValue"))[i-1]).web_element.text
+                products_data_carrefour.append(("producto: " + product_name, "precio: "+ product_price_all))
+            except:
+                print(f"No hemos encontrado el artículo {i}.")
+                products_data_carrefour.append(f"Artículo {i} no encontrado")
         he.kill_browser()
 
     # ############### 5 JUMBO ######################################
     def jumbo_5(product):
-        he.start_chrome("https://www.jumbo.com.ar/")
         print("estamos buscando en Jumbo...")
-        time.sleep(2)
+        he.start_chrome("https://www.jumbo.com.ar/")
+        time.sleep(4)
         search_box5 = he.find_all(he.S(".vtex-styleguide-9-x-input"))[0]
         he.wait_until(search_box5.exists)
         time.sleep(3)
@@ -162,16 +135,15 @@ if start == 'S':
         he.press(he.ENTER)
         time.sleep(5)
         #list_of_cards = he.find_all(he.S(".vtex-search-result-3-x-galleryItem"))
-        try:
-            for i in range (1, 3) :
+        for i in range (1, 3) :
+            try:
                 product_name = (he.find_all(he.S(".vtex-product-summary-2-x-productBrand"))[i-1]).web_element.text
                 product_price = (he.find_all(he.S(".productPrice"))[i-1]).web_element.text
-                products_data_jumbo.append((product_name, product_price))
-        except:
-            print("No hemos encontrado el artículo.")
-            products_data_jumbo.append("Artículo no encontrado")
+                products_data_jumbo.append(("producto: " + product_name, "precio: " + product_price))
+            except:
+                print(f"No hemos encontrado el artículo {i}.")
+                products_data_jumbo.append(f"Artículo {i} no encontrado")
         he.kill_browser()
-
 
     # DEPENDIENDO ZONA, CUALES FUNCIONES
     if location == 'Q':
@@ -186,7 +158,7 @@ if start == 'S':
     elif location == 'S':
         la_reina_2(product)
     elif location == 'O':
-        coto_3(product)
+        #coto_3(product)
         carrefour_4(product)
     elif location == 'N':
         la_gallega_1(product)
@@ -194,9 +166,7 @@ if start == 'S':
         jumbo_5(product)
 
 ################## archivo agregar data #############################
-
     file_name = "informacion_productos_precios.json"
-
     data = {
         "Supermercados": {
             "La Gallega": products_data_la_gallega,
@@ -226,7 +196,6 @@ if start == 'S':
     # BORRAR ARCHIVO
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     delete_file = str(input("Cuando presione ENTER, el archivo será eliminado."))
-
     try:
         os.remove(file_name)
         print("El archivo se eliminó correctamente.")
