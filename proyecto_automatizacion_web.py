e5cca0e8c64f295d4a9e65851fbd6ee9e793bb85
Olivia Luciano <olivialuciano2003@gmail.com>
2023-08-31 15:42:37 -0300

    final enserio

--- proyecto_automatizacion_web.py
+++ proyecto_automatizacion_web.py
@@ -21,7 +21,7 @@ if start == 'S':
     location = str(input("¿En qué zona desea que se ubique el supermercado? (cualquiera: Q, centro: C, sur: S, oeste: O, fisherton: F, norte: N) ")).upper()
     while location != "Q" and location != "C" and location != "S" and location != "O" and location != "F" and location != "N" :
         print("Respuesta inválida. Le preguntaremos de nuevo.")
-        location = str(input("¿En qué zona desea que se ubique el supermercado? (cualquiera: Q, centro: C, sur: S, oeste: O, fisherton: F, norte: N) "))
+        location = str(input("¿En qué zona desea que se ubique el supermercado? (cualquiera: Q, centro: C, sur: S, oeste: O, fisherton: F, norte: N) ")).upper()
     # PRODUCTO
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     product = str(input("Ingrese el producto a buscar: "))
@@ -33,7 +33,7 @@ if start == 'S':
     products_data_carrefour = []
     products_data_jumbo = []
 
-    # ############### 1 LA GALLEGA #################################
+    #### 1 LA GALLEGA
     def la_gallega_1(product):
         print("estamos buscando en La Gallega...")
         he.start_chrome("https://www.lagallega.com.ar/login.asp")
@@ -57,7 +57,7 @@ if start == 'S':
                 products_data_la_gallega.append(f"Artículo {i} no encontrado")
         he.kill_browser()
 
-    # # ############### 2 LA REINA ###################################
+    #### 2 LA REINA 
     def la_reina_2(product):
         print("estamos buscando en La Reina...")
         he.start_chrome("https://www.lareinaonline.com.ar/")
@@ -81,16 +81,16 @@ if start == 'S':
                 products_data_la_reina.append(f"Artículo {i} no encontrado")
         he.kill_browser()
 
-    ############### 3 COTO #######################################
+    #### 3 COTO
     def coto_3(product):
         print("estamos buscando en Coto...")
         he.start_chrome("https://www.cotodigital3.com.ar/sitios/cdigi/")
-        time.sleep(6)
+        time.sleep(8)
         search_box3 = he.find_all(he.S("#atg_store_searchInput"))[0]
         he.wait_until(search_box3.exists)
         he.write(product, into=search_box3)
         he.press(he.ENTER)
-        time.sleep(6)
+        time.sleep(7)
         #list_of_cards = he.find_all(he.S(".clearfix"))
         for i in range (1, 3) :
             try:
@@ -102,16 +102,16 @@ if start == 'S':
                 products_data_coto.append(f"Artículo {i} no encontrado")
         he.kill_browser()
 
-    # ############### 4 CARREFOUR ##################################
+    #### 4 CARREFOUR
     def carrefour_4(product):
         print("estamos buscando en Carrefour...")
         he.start_chrome("https://www.carrefour.com.ar/")
-        time.sleep(8)
+        time.sleep(10)
         search_box4 = he.find_all(he.S(".vtex-styleguide-9-x-input"))[0]
         he.wait_until(search_box4.exists)
         he.write(product, into=search_box4) 
         he.press(he.ENTER)
-        time.sleep(6)
+        time.sleep(8)
         #list_of_cards = he.find_all(he.S(".valtech-carrefourar-product-summary-status-0-x-container"))
         for i in range (1, 3) :
             try:
@@ -123,17 +123,17 @@ if start == 'S':
                 products_data_carrefour.append(f"Artículo {i} no encontrado")
         he.kill_browser()
 
-    # ############### 5 JUMBO ######################################
+    #### 5 JUMBO 
     def jumbo_5(product):
         print("estamos buscando en Jumbo...")
         he.start_chrome("https://www.jumbo.com.ar/")
-        time.sleep(4)
+        time.sleep(7)
         search_box5 = he.find_all(he.S(".vtex-styleguide-9-x-input"))[0]
         he.wait_until(search_box5.exists)
         time.sleep(3)
         he.write(product, into=search_box5)
         he.press(he.ENTER)
-        time.sleep(5)
+        time.sleep(6)
         #list_of_cards = he.find_all(he.S(".vtex-search-result-3-x-galleryItem"))
         for i in range (1, 3) :
             try:
@@ -145,7 +145,7 @@ if start == 'S':
                 products_data_jumbo.append(f"Artículo {i} no encontrado")
         he.kill_browser()
 
-    # DEPENDIENDO ZONA, CUALES FUNCIONES
+    # ZONAS
     if location == 'Q':
         la_gallega_1(product)
         la_reina_2(product)
@@ -158,14 +158,14 @@ if start == 'S':
     elif location == 'S':
         la_reina_2(product)
     elif location == 'O':
-        #coto_3(product)
+        coto_3(product)
         carrefour_4(product)
     elif location == 'N':
         la_gallega_1(product)
         coto_3(product)
         jumbo_5(product)
 
-################## archivo agregar data #############################
+    ###### archivo agregar data ########
     file_name = "informacion_productos_precios.json"
     data = {
         "Supermercados": {
@@ -184,7 +184,7 @@ if start == 'S':
     open_file = str(input("desea ahora abrir el archivo para ver los productos y precios? s/n ")).upper()
     while open_file != 'S' and open_file != 'N':
         print("Respuesta inválida. Le preguntaremos de nuevo.")
-        open_file = str(input("desea ahora abrir el archivo para ver los productos y precios? s/n "))
+        open_file = str(input("desea ahora abrir el archivo para ver los productos y precios? s/n ")).upper()
     if open_file == 'S':
         try:
             subprocess.Popen(['notepad', file_name], shell=True)
@@ -201,11 +201,37 @@ if start == 'S':
         print("El archivo se eliminó correctamente.")
     except:
         print("Ocurrió un error y no se eliminó el archivo.")
+    
+    #BUSCAR POR YOUTUBE
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    yt_video = str(input("¿Desea buscar por youtube una receta del producto que buscó? s/n ")).upper()
+    while yt_video != 'S' and yt_video != 'N':
+        print("Respuesta inválida. Le preguntaremos de nuevo.")
+        yt_video = str(input("¿Desea buscar por youtube una receta del producto que buscó? s/n ")).upper()
+
+    if yt_video == "S":    
+        print("Buscando el video...")
+        he.start_chrome("https://www.youtube.com/@enricorapalin/videos")
+        time.sleep(6)
+        counter = 0
+        try:
+            while counter < 100:
+                video_title = (he.find_all(he.S("#video-title"))[counter]).web_element
+                if product.upper() in (video_title.text).upper():
+                    video_title.click()
+                    print("Reproduciendo la receta...")
+                    break
+                else:
+                    he.scroll_down(50)
+                    he.scroll_down(50)
+                    he.scroll_down(50)
+                    counter += 1
+        except:
+            print("No se ha encontrado nungun video receta del producto que buscó.")
     print("---------------------------------")
     print("Espero que te hayamos ayudado <3")
     print("---------------------------------")
     print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-
 else:
     print("---------------------------------")
     print("Bueno, gracias por elegirnos(?")
